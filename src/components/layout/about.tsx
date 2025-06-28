321930df584ddbf95c1c254dae6d4a569fbf5e02
khazeez <cakirul211001@gmail.com>
2025-06-28 16:01:47 +0700

    updated

--- src/components/layout/about.tsx
+++ src/components/layout/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className='tracking-widest gap-y-5 py-10 space-y-5'>
       <h1 id='about' className='text-5xl font-bold scroll-mt-24'>
-        Know Me Better!
+        Know me better!
       </h1>
       <Glassmorphin className='p-4 z-10'>
         <p className=' tracking-wider text-normal'>{about.paragraphs}</p>
