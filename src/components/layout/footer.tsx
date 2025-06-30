21cb94ccf190fd1f4390215fd136f5e4a1257aae
Khoirul Aziz <cakirul211001@gmail.com>
2025-06-30 11:05:08 +0700

    updated

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 return (
-  <div className='flex justify-center items-center text-gray-500 pt-10'>
+  <div className='flex justify-center items-center text-gray-500'>
     <p>Build with Next JS, tailwind and CMS with Strapi ❤️</p>
   </div>
 );
